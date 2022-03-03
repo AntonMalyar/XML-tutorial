91878b0086669b78aa71f23f021b80543ea726de
Anton Malyar <antn.mlr@gmail.com>
2022-03-03 13:43:25 +0200

    create create test - Create Document object from test.xml and get root element of object Document

--- src/test/java/XML_tutorial_test.java
+++ src/test/java/XML_tutorial_test.java
@@ -1,25 +1,35 @@
 import javax.xml.parsers.*;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
 
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class XML_tutorial_test {
+    private static DocumentBuilderFactory factory;
+    private static DocumentBuilder builder;
+    private static Document document;
+    
     @BeforeAll
-    public void preparation() throws ParserConfigurationException, IOException, SAXException {
+    public static void preparation() throws ParserConfigurationException, IOException, SAXException {
         //Get Document Builder
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
+        factory = DocumentBuilderFactory.newInstance();
+        builder = factory.newDocumentBuilder();
         //Build Document
-        Document document = builder.parse(new File("test.xml"));
+        document = builder.parse(new File("C:\\Users\\fuckyou\\Desktop\\VS work space\\XML-tutorial\\src\\test\\resources\\test.xml"));
         //Normalize the XML Structure; It's just too important !!
         document.getDocumentElement().normalize();
-        System.out.print("preparation");
-
+    }
+    @Test
+    void testToGetRootElementOfDocument() {
+        Element root = document.getDocumentElement();
+        String rootElementName = root.getNodeName();
+        Assertions.assertEquals("employees", rootElementName);
     }
     
 }
